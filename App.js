b30a0005e0c2617113a26e35dfadfa10b32bdd4f
Gaurab Chhetri <contact@gaurabchhetri.com.np>
2022-08-06 16:03:38 +0545

    added title length validation and kbd dismiss

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import { useState } from "react";
-import { StyleSheet, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
+  StatusBar,
+} from "react-native";
 import AddTodo from "./components/AddTodo";
 import Header from "./components/Header";
 import TodoItem from "./components/TodoItem";
@@ -14,28 +22,39 @@ export default function App() {
   };
 
   const submitHandler = (text) => {
-    setTodos((prev) => {
-      return [{ text: text, key: Date.now().toString() }, ...prev];
-    });
+    if (text.length > 3) {
+      setTodos((prev) => {
+        return [{ text: text, key: Date.now().toString() }, ...prev];
+      });
+      Keyboard.dismiss();
+    } else {
+      Alert.alert("OOPS!", "ToDos must be over 3 chars long", [{ text: "OK" }]);
+    }
   };
   return (
-    <View style={styles.container}>
-      {/* header  */}
-      <Header />
-      <View style={styles.content}>
-        {/* to form  */}
-        <AddTodo submitHandler={submitHandler} />
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem={({ item }) => (
-              <TodoItem item={item} pressHandler={pressHandler} />
-            )}
-          />
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.container}>
+        {/* header  */}
+        <Header />
+        <View style={styles.content}>
+          {/* to form  */}
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
         </View>
+        <StatusBar />
       </View>
-      {/* <StatusBar /> */}
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
